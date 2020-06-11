63feb4c3135186a6b195cc1c9f7925c3322b12c4
kamiazya <yuki@kamiazya.tech>
2020-06-11 19:51:10 +0900

    fix bugs

--- src/utils/svg.ts
+++ src/utils/svg.ts
@@ -4,6 +4,9 @@ import path from 'path';
 import { JSDOM } from 'jsdom';
 import imageDataURI from 'image-data-uri';
 
+const SVG_HEADER =
+  '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"\n "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n';
+
 export function svgToImageDataURL(svg: string, imageBasePath = ''): string {
   const dom = new JSDOM();
   dom.window.document.querySelector('body')!.innerHTML = svg;
@@ -19,10 +22,9 @@ export function svgToImageDataURL(svg: string, imageBasePath = ''): string {
       target.setAttribute('xlink:href', imageSrc);
     }
   });
-  const buffer = Buffer.from(
-    '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"\n "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n',
-  );
-  buffer.write(dom.window.document.querySelector('body')!.innerHTML);
+
+  const imageEmbedSVG = dom.window.document.querySelector('body')!.innerHTML;
+  const buffer = Buffer.from(SVG_HEADER + imageEmbedSVG, 0, SVG_HEADER.length + imageEmbedSVG.length);
 
   return imageDataURI.encode(buffer, 'svg+xml');
 }
